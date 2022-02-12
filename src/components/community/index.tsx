ab6a5ed58729713193fb513770ff25cfcbe54994
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-02-12 14:34:07 -0300

    update manifest and favicons

---
+++ src/components/community/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+export const Community = () => {
+  return <div id={styles.Community}>Community</div>;
+};
+
+export default Community;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,8 @@ import Features from "../../components/features";
 import Produts from "../../components/products";
 import Roadmap from "../../components/roadmap";
 import WorkWithUs from "../../components/workWithUs";
+import Community from "../../components/community";
+import Footer from "../../components/footer";
 
 const Home = () => {
   return (
@@ -12,6 +14,7 @@ const Home = () => {
       <Produts />
       <Roadmap />
       <WorkWithUs />
+      <Community />
     </div>
   );
 };
