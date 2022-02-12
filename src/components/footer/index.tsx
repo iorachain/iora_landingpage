6fea5510bc7e1de465b178c57109ed2480064efb
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-02-12 20:52:08 -0300

    footer centered

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,34 +7,36 @@ const Footer = () => {
   return (
     <div id={styles.Footer}>
       <Row>
-        <Col md={4} xs={12} className={styles.footerLogo}>
+        <Col md={4} sm={12} className={styles.footerLogo}>
           <Image height={isMobile ? 40 : 100} src={IoraChain} alt="IoraChain" />
-          <span className={styles.logoSubtitle}>
+          <span className={styles.logoTitle}>
             <b>iora</b>chain
           </span>
           <MobileView>
-            <span>Blockchain for LATAM Users. Your new home.</span>
+            <span className={styles.logoSubTitle}>
+              Blockchain for LATAM Users. Your new home.
+            </span>
           </MobileView>
         </Col>
         <BrowserView>
-          <Col md={2} xs={12} className={styles.footerDapps}>
+          <Col md={2} sm={12} className={styles.footerDapps}>
             <b>Dapps</b>
             <span>IoraPay</span>
             <span>Praora</span>
           </Col>
-          <Col md={2} xs={12} className={styles.footerDocumentation}>
+          <Col md={2} sm={12} className={styles.footerDocumentation}>
             <b>Documentations</b>
             <span>About Us</span>
             <span>Team</span>
             <span>Whitepaper</span>
           </Col>
-          <Col md={2} xs={12} className={styles.footerLinks}>
+          <Col md={2} sm={12} className={styles.footerLinks}>
             <b>Links</b>
             <span>Features</span>
             <span>Roadmap</span>
             <span>Token Sale</span>
           </Col>
-          <Col md={2} xs={12} className={styles.footerContactUs}>
+          <Col md={2} sm={12} className={styles.footerContactUs}>
             <b>Contact Us</b>
             <span>Discord</span>
             <span>Github</span>
