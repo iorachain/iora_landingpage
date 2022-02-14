520b335b78a321e97e0b1c6d334df0a9bac3fe81
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-02-14 20:12:12 -0300

    workingTogether

--- src/components/workWithUs/index.tsx
+++ src/components/workWithUs/index.tsx
@@ -4,24 +4,29 @@ import { Image } from "react-bootstrap";
 import HireTeam from "../../assets/imgs/hire.svg";
 import Button from "@mui/material/Button";
 import { isMobile } from "react-device-detect";
+import { useTranslation } from "react-i18next";
 
 const WorkWithUs = () => {
+  const { t } = useTranslation();
+
   return (
     <section id={styles.WorkWithUs}>
-      <div className={styles.titleWorkWithUs}>Enjoy our team</div>
+      <div className={styles.titleWorkWithUs}>
+        {t("workingWIthUs.enjoyOurTeam")}
+      </div>
       <Row>
         <Col>
           <div className={styles.figureHireTeam}>
             <Image
               width={isMobile ? 300 : 400}
               src={HireTeam}
-              alt="Enjoy our team"
+              alt={t("workingWIthUs.enjoyOurTeam")}
             />
           </div>
         </Col>
         <Col className={styles.sectionWorkWithUs}>
           <div className={styles.subTitleWorkWithUs}>
-            START WORKING WITH OUR COMPANY AND PRODUCTS
+            {t("workingWIthUs.getStartedWorkingTogether")}
           </div>
           <div className={styles.getStartedButton}>
             <Button
@@ -29,7 +34,7 @@ const WorkWithUs = () => {
               size="large"
               className={styles.explorerButton}
             >
-              GET STARTED
+              {t("workingWIthUs.getStarted")}
             </Button>
           </div>
         </Col>
