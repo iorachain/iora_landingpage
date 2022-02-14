501ee492ade46a2a639ffc316bd9cc8f7e8cfd89
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-02-14 20:33:58 -0300

    footer translated

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,57 +9,51 @@ const Footer = () => {
 
   return (
     <div id={styles.Footer}>
-      <Container>
-        <Row>
-          <div className={styles.footerDirection}>
-            <Col md={4} className={styles.footerLogo}>
-              <Image
-                width={isMobile ? 15 : 30}
-                src={IoraChain}
-                alt="IoraChain"
-              />
-              <span className={styles.logoTitle}>
-                <b>iora</b>chain
+      <Row>
+        <div className={styles.footerDirection}>
+          <Col md={4} className={styles.footerLogo}>
+            <Image width={isMobile ? 80 : 30} src={IoraChain} alt="IoraChain" />
+            <span className={styles.logoTitle}>
+              <b>iora</b>chain
+            </span>
+            <MobileView>
+              <span className={styles.logoSubTitle}>
+                {t("dapps.ioraChainTitle")}
               </span>
-              <MobileView>
-                <span className={styles.logoSubTitle}>
-                  {t("dapps.ioraChainTitle")}
-                </span>
-              </MobileView>
-            </Col>
-            <Col md={8}>
-              <BrowserView>
-                <div className={styles.footerDirection}>
-                  <Col md={3} sm={12} className={styles.footerDapps}>
-                    <b>{t("footer.dapps")}</b>
-                    <span>IoraPay</span>
-                    <span>Praora</span>
-                  </Col>
-                  <Col md={3} className={styles.footerDocumentation}>
-                    <b>{t("footer.documentations")}</b>
-                    <span>{t("footer.aboutUs")}</span>
-                    <span>{t("footer.team")}</span>
-                    <span>{t("footer.whitepaper")}</span>
-                  </Col>
-                  <Col md={3} className={styles.footerLinks}>
-                    <b>Links</b>
-                    <span>{t("footer.features")}</span>
-                    <span>{t("footer.roadmap")}</span>
-                    <span>{t("footer.tokenSale")}</span>
-                  </Col>
-                  <Col md={3} className={styles.footerContactUs}>
-                    <b>{t("footer.contactUs")}</b>
-                    <span>Discord</span>
-                    <span>Github</span>
-                    <span>Linkedin</span>
-                    <span>Instagram</span>
-                  </Col>
-                </div>
-              </BrowserView>
-            </Col>
-          </div>
-        </Row>
-      </Container>
+            </MobileView>
+          </Col>
+          <Col md={8}>
+            <BrowserView>
+              <div className={styles.footerDirection}>
+                <Col md={3} sm={12} className={styles.footerDapps}>
+                  <b>{t("footer.dapps")}</b>
+                  <span>IoraPay</span>
+                  <span>Praora</span>
+                </Col>
+                <Col md={3} className={styles.footerDocumentation}>
+                  <b>{t("footer.documentations")}</b>
+                  <span>{t("footer.aboutUs")}</span>
+                  <span>{t("footer.team")}</span>
+                  <span>{t("footer.whitepaper")}</span>
+                </Col>
+                <Col md={3} className={styles.footerLinks}>
+                  <b>Links</b>
+                  <span>{t("footer.features")}</span>
+                  <span>{t("footer.roadmap")}</span>
+                  <span>{t("footer.tokenSale")}</span>
+                </Col>
+                <Col md={3} className={styles.footerContactUs}>
+                  <b>{t("footer.contactUs")}</b>
+                  <span>Discord</span>
+                  <span>Github</span>
+                  <span>Linkedin</span>
+                  <span>Instagram</span>
+                </Col>
+              </div>
+            </BrowserView>
+          </Col>
+        </div>
+      </Row>
     </div>
   );
 };
