7e31942cc4a957f8510c03a4cc2e270c648bcb87
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-03-14 12:35:43 -0300

    iorachain header updated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -104,7 +104,7 @@ const Header = () => {
                   <Button
                     variant="text"
                     className={styles.btnLinks}
-                    href="https://pancakeswap.finance/swap?outputCurrency=0xBdE32737D0AF849b1d3161aB2b881339b8CC1707"
+                    href="https://pancakeswap.finance/swap?outputCurrency=0xB70E1255446822E3B8dfe2d2c11fa1c9cb4a9c53"
                   >
                     <span>{t("header.tokenSale")}</span>
                   </Button>
